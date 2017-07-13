d3aecc32fea6ba8a54bbd96ca3fab1a1600b7a99
chenms-m2 <hellochenms@163.com>
2017-07-13 23:39:55 +0800

    feat: init

---
+++ MHelloLabelTextButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MHelloLabelTextButton
+//
+//  Created by chenms on 17/7/13.
+//  Copyright © 2017年 chenms.m2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MHelloLabelTextButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MHelloLabelTextButton
+//
+//  Created by chenms on 17/7/13.
+//  Copyright © 2017年 chenms.m2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
